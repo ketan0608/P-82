4f3068c0aa9703d52c3d2b0d725dedaa8007403b
ketan0608 <76109939+ketan0608@users.noreply.github.com>
2022-03-05 13:23:43 +0530

    Add files via upload

---
+++ p-82/screens/CreatePost.js
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+export default class CreatePost extends React.Component {
+  render(){
+  return <Text>Hello Create</Text>
+  }
+}
